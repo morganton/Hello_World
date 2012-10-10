14bd82226d8da37c53bf1cd7e8156d8569da9840
Dan Conley <electrolaw@gmail.com>
2012-10-09 20:40:25 -0700

    Second Commit

---
+++ Hello_World.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int  main()
+{
+   cout << "Hello World! and Good Bye\n";
+   return 0;
+}
+/*------------------------- paste of run ----------------------------------------------
+Hello World!
+Press any key to continue . . .
+---------------------------------------------------------------------------------------*/
